2ef23c8dd32d65ff974ed49fb43965d66c40325c
hreinn <hreinngy@gmail.com>
2023-01-13 20:04:41 +0100

    Adds color variable and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      blue: '#755CDE',
+      yellow: '#F6A560',
+      pink: '#F39E9E',
+      orange: '#EB7565',
+      green: '#61C4B7',
+      purple: '#552049',
+      black: '#030303',
+      gray: '#7A746E',
+      white: '#FFF7F0',
+    },
+    extend: {
+      fontFamily: {
+        plusjakartasans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
