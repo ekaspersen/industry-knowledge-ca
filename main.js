083da11effdcb80a3eb44151f7d11f0453360269
hreinn <hreinngy@gmail.com>
2023-01-18 22:32:57 +0100

    Adds My work carousel for desktop. Adds 'lint' to scripts in package.json. Turns off conflicting 'operator-linebreak' rule in eslint

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const imgGalleryContainer = document.querySelector('#my-work-img-gallery');
 const imgSlideControls = document.querySelector('#img-slide');
 let scrollPos = 3;
-
 function handleScrollPos(num) {
   imgGalleryContainer.querySelector(`img:nth-child(${num})`).scrollIntoView({ inline: 'center' });
 }
 
 handleScrollPos(scrollPos);
 imgSlideControls.addEventListener('click', (event) => {
-  if (event.target.id === 'arrow-right') scrollPos = scrollPos < 5 ? (scrollPos += 1) : (scrollPos = 1);
-  if (event.target.id === 'arrow-left') scrollPos = scrollPos > 1 ? (scrollPos -= 1) : (scrollPos = 5);
-  handleScrollPos(scrollPos);
+  if (window.innerWidth < 1024) {
+    if (event.target.id === 'arrow-right') scrollPos = scrollPos < 5 ? (scrollPos += 1) : (scrollPos = 1);
+    if (event.target.id === 'arrow-left') scrollPos = scrollPos > 1 ? (scrollPos -= 1) : (scrollPos = 5);
+    handleScrollPos(scrollPos);
+  } else {
+    imgGalleryContainer.scrollLeft =
+      imgGalleryContainer.scrollLeft === 1244
+        ? (imgGalleryContainer.scrollLeft = 0)
+        : (imgGalleryContainer.scrollLeft = 1244);
+  }
 });
